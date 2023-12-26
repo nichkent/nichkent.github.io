50b2cdee651e11b33fb9e38434f207a9ec1ae80a
Nicholas Ian Kent <nikent@uvm.edu>
2023-12-26 13:14:15 -0500

    Initial commit.

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  include header.php
+?>
+
+<main>
+   <article>
+     <h1>Nicholas Kent's Portfolio</h1>
+     <body>
+	
+     </body>
+   </article>
+</main>
+
+<?php
+  include footer.php
+?>
